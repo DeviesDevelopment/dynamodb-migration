72a72277adeb3d5e5650e1da4757c1b42ec12815
Rickard Andersson <rickard.andersson@wirelesscar.com>
2021-03-18 11:59:01 +0100

    Add migration script

--- README.md
+++ README.md
@@ -3,22 +3,37 @@
 Example code for migrating data between tables using DynamoDB Streams.
 
 ## Usage
+
 Follow these instructions if you want to play around with the example code.
 
 ### Deployment
+
 Make sure you have the [SAM CLI tool](https://aws.amazon.com/serverless/sam/) installed.
 
     sam deploy --stack-name dynamodb-migration-example --capabilities CAPABILITY_IAM
 
 ### Populate data
+
 Add any number of items to the `OriginalBooksTable`. Each item should at least contain the following properties:
 
 ```json
 {
-    "isbn": "some-isbn",
-    "author": "some-author",
-    "title": "some-title"
+  "isbn": "some-isbn",
+  "author": "some-author",
+  "title": "some-title"
 }
 ```
 
 ### Run migration script
+
+If you want to try out the migration script (see step 3 in the blog post).
+
+First, delete every item in the `TargetBooksTable`.
+
+To run the migration script:
+
+    cd migrate-data
+    npm install
+    node app.js
+
+This will add a `migrate` property to every item in `OriginalBooksTable`, in order to trigger the migration stream even for items that were added before the stream was in place.
---
+++ migrate-data/app.js
@@ -0,0 +1,52 @@
+const AWS = require('aws-sdk');
+
+const dynamodb = new AWS.DynamoDB();
+
+const originalTableName = "dynamodb-migration-example-OriginalBooksTable";
+let items = await scanWithPagination(originalTableName);
+
+let promises = [];
+items.forEach(item => {
+  promises.push(migrateItem(item, originalTableName));
+});
+await Promise.all(promises);
+
+const scanWithPagination = async (tableName) => {
+  let items = [];
+  let lastEvaluatedKey;
+  do {
+    const params = {
+      TableName: tableName
+    };
+    if (lastEvaluatedKey) {
+      params.ExclusiveStartKey = lastEvaluatedKey;
+    }
+    const res = await dynamodb.scan(params).promise();
+    lastEvaluatedKey = res.LastEvaluatedKey;
+    console.log("Scanned page! Last evaluated key: ", lastEvaluatedKey);
+    items = items.concat(res.Items);
+  } while (lastEvaluatedKey);
+  console.log("Found " + items.length + " items");
+  return items;
+}
+
+const migrateItem = (item, tableName) => {
+  const params = {
+    ExpressionAttributeNames: {
+      "#M": "migrate",
+    },
+    ExpressionAttributeValues: {
+      ":m": {
+        S: new Date().toISOString()
+      },
+    },
+    Key: {
+      "isbn": {
+        S: item.isbn.S
+      },
+    },
+    TableName: tableName,
+    UpdateExpression: "SET #M = :m"
+  };
+  return dynamodb.updateItem(params).promise();
+}
